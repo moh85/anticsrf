89422afb5df6b84e00d33f21526fcd0c45fc92a0
Mohammad Atwi <mega.web.own@gmail.com>
2021-07-31 12:12:19 +0300

    AJAX setup change
    Changed AJAX setup so it can support any form of data sent to server like JSON

--- anicsrf.js
+++ anicsrf.js
@@ -1,2 +1,17 @@
-/**** antiCSRF v1.0 by Mohammad Atwi ****/
-function antiCSRF(data, option = { target: 'form'}){ function addInputs(target){ $.each(data, function(name, val){ $('[name="'+name+'"]', target).remove(); $('<input/>').attr('type', 'hidden').attr('name', name).attr('value', val).appendTo(target); }); } $.ajaxSetup({ data: data }); addInputs(option.target); $(document).on('submit', option.target, function(){ addInputs(this); return true; }); }
+/**** antiCSRF v1.1 by Mohammad Atwi ****/
+function antiCSRF(data, option = {target: 'form'}) {
+    function addInputs(target) {
+        $.each(data, function(name, val) {
+            $('[name="' + name + '"]', target).remove();
+            $('<input/>').attr('type', 'hidden').attr('name', name).attr('value', val).appendTo(target);
+        });
+    }
+    $.ajaxSetup({
+    headers: data
+    });
+    addInputs(option.target);
+    $(document).on('submit', option.target, function() {
+        addInputs(this);
+        return true;
+    });
+}
